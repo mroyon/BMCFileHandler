f3a61ef0b3728e531413b4da0182ab80e0f97755
Mahmudur Rahman <mroyon@gmail.com>
2024-01-27 22:42:39 +0300

    ts

--- BMCFileMangement/MainWindows.Designer.cs
+++ BMCFileMangement/MainWindows.Designer.cs
@@ -30,35 +30,35 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindows));
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statStrip = new System.Windows.Forms.StatusStrip();
             this.currentDateTimeStip = new System.Windows.Forms.ToolStripStatusLabel();
             this.statusDateTime = new System.Windows.Forms.Timer(this.components);
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
             this.mnChangePass = new System.Windows.Forms.ToolStripMenuItem();
             this.mnChangePassword = new System.Windows.Forms.ToolStripMenuItem();
             this.mnLogOut = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.statusStrip1.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
+            this.statStrip.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.toolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // statusStrip1
+            // statStrip
             // 
-            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.currentDateTimeStip});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
-            this.statusStrip1.TabIndex = 1;
-            this.statusStrip1.Text = "statusStrip1";
+            this.statStrip.Location = new System.Drawing.Point(0, 422);
+            this.statStrip.Name = "statStrip";
+            this.statStrip.Size = new System.Drawing.Size(800, 28);
+            this.statStrip.TabIndex = 1;
+            this.statStrip.Text = "statusStrip1";
             // 
             // currentDateTimeStip
             // 
             this.currentDateTimeStip.Name = "currentDateTimeStip";
-            this.currentDateTimeStip.Size = new System.Drawing.Size(0, 15);
+            this.currentDateTimeStip.Size = new System.Drawing.Size(0, 21);
             // 
             // statusDateTime
             // 
@@ -66,16 +66,16 @@
             this.statusDateTime.Interval = 1000;
             this.statusDateTime.Tick += new System.EventHandler(this.statusDateTime_Tick);
             // 
-            // menuStrip1
+            // menuStrip
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnChangePass});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
-            this.menuStrip1.TabIndex = 3;
-            this.menuStrip1.Text = "File";
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "File";
             // 
             // mnChangePass
             // 
@@ -99,16 +99,16 @@
             this.mnLogOut.Text = "Log Out";
             this.mnLogOut.Click += new System.EventHandler(this.mnLogOut_Click);
             // 
-            // toolStrip1
+            // toolStrip
             // 
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 33);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(800, 41);
-            this.toolStrip1.TabIndex = 4;
-            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip.Location = new System.Drawing.Point(0, 33);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(800, 41);
+            this.toolStrip.TabIndex = 4;
+            this.toolStrip.Text = "toolStrip1";
             // 
             // toolStripButton1
             // 
@@ -124,22 +124,22 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.statStrip);
+            this.Controls.Add(this.menuStrip);
             this.IsMdiContainer = true;
-            this.MainMenuStrip = this.menuStrip1;
+            this.MainMenuStrip = this.menuStrip;
             this.Name = "MainWindows";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BMC File Management System";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.MainWindows_Load);
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
+            this.statStrip.ResumeLayout(false);
+            this.statStrip.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,12 +147,12 @@
 
         #endregion
 
-        private StatusStrip statusStrip1;
+        private StatusStrip statStrip;
         private ToolStripStatusLabel currentDateTimeStip;
         private System.Windows.Forms.Timer statusDateTime;
-        private MenuStrip menuStrip1;
+        private MenuStrip menuStrip;
         private ToolStripMenuItem mnChangePass;
-        private ToolStrip toolStrip1;
+        private ToolStrip toolStrip;
         private ToolStripMenuItem mnChangePassword;
         private ToolStripMenuItem mnLogOut;
         private ToolStripButton toolStripButton1;
